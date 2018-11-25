ac116415fca0aa8df0e102fa9012bd6d4a7c77ce
Filip Staniul <filip.staniul@hotmail.com>
2018-11-25 15:21:09 +0100

    Errors fixup Fixed errors in files store state.

--- src/renderer/store/modules/files.js
+++ src/renderer/store/modules/files.js
@@ -3,25 +3,25 @@ import Vue from 'vue'
 export default {
     state: [],
     mutations: {
-        'add-file': (state, file) => {
+        'add-file' (state, file) {
             state.push(file)
         },
-        'update-file': (state, { id, data }) => {
+        'update-file' (state, { id, data }) {
             const index = state.findIndex(f => f.id === id)
             Vue.set(state, index, { ...state[index], ...data })
         }
     },
     actions: {
-        'update-file': ({commit}, payload) {
+        'update-file' ({ commit }, payload) {
             commit('update-file', payload)
         },
-        'new-file': ({commit}, payload) {
+        'new-file' ({ commit }, payload) {
             commit('add-file', payload)
         }
     },
     getters: {
-        'getFileById': (state) => {
-            return (id) => state.filter(f => f.id === id)
+        getFileById: state => {
+            return id => state.filter(f => f.id === id)
         }
     }
 }
